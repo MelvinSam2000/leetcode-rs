4643dab8ead38f77724914ef8cc3ea95bcc98dea
MelvinSam2000 <melvinsamxd@gmail.com>
2022-07-24 16:18:11 -0400

    198 Size optimization

--- src/q198.rs
+++ src/q198.rs
@@ -1,23 +1,27 @@
-use std::cmp::max;
-
 /*
     198 - House Robber
     Time: O(n)
-    Space: O(n)
+    Space: O(1)
 */
 pub fn rob(nums: Vec<i32>) -> i32 {
+    use std::cmp::max;
     let n = nums.len();
     if n == 1 {
         return nums[0];
     }
-
-    let mut dp = vec![0; n];
-    dp[0] = nums[0];
-    dp[1] = max(nums[0], nums[1]);
-    for i in 2..n {
-        dp[i] = max(dp[i - 1], nums[i] + dp[i - 2])
+    if n == 2 {
+        return max(nums[0], nums[1]);
     }
-    dp[n - 1]
+
+    let mut dp_2 = nums[0];
+    let mut dp_1 = max(nums[0], nums[1]);
+    let mut dp = 0;
+    for i in 2..n {
+        dp = max(dp_1, nums[i] + dp_2);
+        dp_2 = dp_1;
+        dp_1 = dp;
+    }
+    dp
 }
 
 #[test]
