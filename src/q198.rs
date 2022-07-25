fc685c7f0f01fea4e6a6dc0cb28804ba87e7e3c4
MelvinSam2000 <melvinsamxd@gmail.com>
2022-07-24 21:08:55 -0400

    746 Size optimization

--- src/q198.rs
+++ src/q198.rs
@@ -24,6 +24,24 @@ pub fn rob(nums: Vec<i32>) -> i32 {
     dp
 }
 
+//Previous version (Space O(n))
+fn _rob(nums: Vec<i32>) -> i32 {
+    use std::cmp::max;
+        let n = nums.len();
+        
+        if n == 1 {
+            return nums[0];
+        }
+        
+        let mut dp = vec![0; n];
+        dp[0] = nums[0];
+        dp[1] = max(nums[0], nums[1]);
+        for i in 2..n {
+            dp[i] = max(dp[i - 1], nums[i] + dp[i - 2])
+        }
+        dp[n - 1]
+}
+
 #[test]
 fn t1() {
     assert_eq!(rob(vec![1, 2, 3, 1]), 4);
--- src/q746.rs
+++ src/q746.rs
@@ -1,9 +1,30 @@
 /*
     746 - Min Cost Climbing Stairs
     Time: O(n)
-    Space: O(n)
+    Space: O(1)
 */
 pub fn min_cost_climbing_stairs(cost: Vec<i32>) -> i32 {
+    use std::cmp::min;
+    let n = cost.len();
+    
+    if n == 2 {
+        return min(cost[0], cost[1]);
+    }
+
+    let mut dp_2 = cost[0];
+    let mut dp_1 = cost[1];
+    let mut dp = 0;
+
+    for i in 2..n {
+        dp = cost[i] + min(dp_1, dp_2);
+        dp_2 = dp_1;
+        dp_1 = dp;
+    }
+    min(dp, dp_2)
+}
+
+// Previous version (Space O(n))
+fn _min_cost_climbing_stairs(cost: Vec<i32>) -> i32 {
     use std::cmp::min;
     let n = cost.len();
     let mut dp = vec![0; n];
