3f93ca7ded50c98c4a645c732713a42ab005a652
MelvinSam2000 <melvinsamxd@gmail.com>
2022-07-25 02:01:10 -0400

    206 Reverse linked list

--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,11 @@
 #![allow(clippy::needless_range_loop)]
 
+pub mod linkedlist;
 pub mod q1;
 pub mod q1137;
 pub mod q125;
 pub mod q198;
+pub mod q206;
 pub mod q213;
 pub mod q217;
 pub mod q5;
---
+++ src/q206.rs
@@ -0,0 +1,23 @@
+use crate::linkedlist::ListNode;
+
+/*
+    206 - Reverse Linked List
+    Time: O(n)
+    Space: O(1), O(n) with recursion stack
+*/
+pub fn reverse_list(head: Option<Box<ListNode>>) -> Option<Box<ListNode>> {
+    helper(head, None)
+}
+
+fn helper(
+    head: Option<Box<ListNode>>,
+    reversed: Option<Box<ListNode>>
+) -> Option<Box<ListNode>> {
+    if let Some(mut node) = head {
+        let new_head = node.next.take();
+        node.next = reversed;
+        helper(new_head, Some(node))
+    } else {
+        reversed
+    }
+}
