b218cc69a978db3cab67881decc5fe6e671b542d
MelvinSam2000 <melvinsamxd@gmail.com>
2022-07-24 22:49:39 -0400

    125 valid palindrome

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,7 @@
 #![allow(clippy::needless_range_loop)]
 
 pub mod q1137;
+pub mod q125;
 pub mod q198;
 pub mod q213;
 pub mod q509;
---
+++ src/q125.rs
@@ -0,0 +1,39 @@
+/*
+    125 - Valid Palindrome
+    Time: O(n)
+    Space: O(n)
+    Note: Could try to optimize space by not creating a second copy of string without alphanumeric characters.
+    In that case space would be O(1)
+*/
+
+pub fn is_palindrome(s: String) -> bool {
+    let s = s
+        .chars()
+        .filter(|c| c.is_alphanumeric())
+        .collect::<String>()
+        .to_lowercase();
+    let s = s.as_bytes();
+    let n = s.len();
+    if n <= 1 {
+        return true;
+    }
+    for i in 0..(n/2+1) {
+        if s[i] != s[n - i - 1] {
+            return false;
+        }
+    }
+    true
+}
+
+#[test]
+fn t1() {
+    let tcases = [
+        ("A man, a plan, a canal: Panama", true),
+        ("race a car", false),
+        (" ", true),
+        ("  ", true),
+    ];
+    for (param, out) in tcases.into_iter() {
+        assert_eq!(is_palindrome(param.to_owned()), out);
+    }
+}
