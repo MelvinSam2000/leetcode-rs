a9e4faedb2efe66903000cbe3919935ec8b095ac
MelvinSam2000 <melvinsamxd@gmail.com>
2022-07-26 15:35:03 -0400

    45 Jump Game II

---
+++ src/q45.rs
@@ -0,0 +1,27 @@
+/*
+    45 - Jump Game II
+    Time: O(n)
+    Space: O(1)
+*/
+pub fn jump(nums: Vec<i32>) -> i32 {
+    use std::cmp::max;
+    let n = nums.len(); 
+    if n == 1 {
+        return 0;
+    }
+    let mut jumps = 0;
+    let mut covered = 0;
+    let mut max_covered = 0;
+    for i in 0..n {
+        if i == covered {
+            covered = max(max_covered, i + nums[i] as usize);
+            jumps += 1;
+            if covered >= n - 1 {
+                return jumps;
+            }
+        } else {
+            max_covered = max(max_covered, i + nums[i] as usize)
+        }
+    }
+    jumps
+}
