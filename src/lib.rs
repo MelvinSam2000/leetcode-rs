cb4b74fb56f3d0f84e91fe026e0181331634dafa
MelvinSam2000 <melvinsamxd@gmail.com>
2022-08-17 15:19:01 -0400

    771 Jewels and Stones

--- src/lib.rs
+++ src/lib.rs
@@ -95,6 +95,7 @@ pub mod q733;
 pub mod q740;
 pub mod q743;
 pub mod q746;
+pub mod q771;
 pub mod q79;
 pub mod q796;
 pub mod q9;
---
+++ src/q771.rs
@@ -0,0 +1,14 @@
+/*
+    771 - Num of jewels in stones
+    Time: O(n + m)
+    Space: O(n)
+*/
+pub fn num_jewels_in_stones(jewels: String, stones: String) -> i32 {
+    use std::collections::HashSet;
+
+    let jewels: HashSet<char> = HashSet::from_iter(jewels.chars().into_iter());
+    stones
+        .chars()
+        .filter(|stone| jewels.contains(stone))
+        .count() as i32
+}
