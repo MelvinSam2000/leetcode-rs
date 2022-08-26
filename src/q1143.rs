f245c1d08deadb6baa729cfa00af5c750e78348d
MelvinSam2000 <melvinsamxd@gmail.com>
2022-08-25 21:24:57 -0400

    Revisit 1143 Longest Common Subsequence

--- src/q1143.rs
+++ src/q1143.rs
@@ -1,54 +1,49 @@
 /*
     1143 - Longest Common Subsequence
-    Time: O(n*m)
-    Space: O(m)
-    Note: This is the space optimized, but less readable version
+    Time: O(m*n)
+    Space: O(n)
 */
-pub fn longest_common_subsequence(text1: String, text2: String) -> i32 {
-    use std::mem::swap;
-    let s1 = text1.as_bytes();
-    let s2 = text2.as_bytes();
-    let m = s1.len() + 1;
-    let n = s2.len() + 1;
-    let mut dp1 = vec![0; m];
-    let mut dp2 = vec![0; m];
-
-    for j in 1..n {
-        for i in 1..m {
-            dp2[i] = if s1[i - 1] == s2[j - 1] {
-                1 + dp1[i - 1]
+pub fn longest_common_subsequence(s1: String, s2: String) -> i32 {
+    let s1 = s1.as_bytes();
+    let s2 = s2.as_bytes();
+    let m = s1.len();
+    let n = s2.len();
+    let mut dp = [vec![0; n + 1], vec![0; n + 1]];
+    for i in (0..m).rev() {
+        for j in (0..n).rev() {
+            dp[0][j] = if s1[i] == s2[j] {
+                1 + dp[1][j + 1]
             } else {
-                dp2[i - 1].max(dp1[i])
-            }
+                dp[0][j + 1].max(dp[1][j])
+            };
         }
-        swap(&mut dp1, &mut dp2);
+        dp.swap(0, 1);
     }
-    dp1[m - 1]
+    dp[1][0]
 }
 
 /*
     1143 - Longest Common Subsequence
     Time: O(n*m)
     Space: O(n*m)
-    Note: Space can be optimized to O(min(n, m))
+    Note: Can be space optimized..
 */
-pub fn longest_common_subsequence_v2(text1: String, text2: String) -> i32 {
-    let s1 = text1.as_bytes();
-    let s2 = text2.as_bytes();
-    let m = s1.len() + 1;
-    let n = s2.len() + 1;
-    let mut dp = vec![vec![0; n]; m];
-
-    for i in 1..m {
-        for j in 1..n {
-            dp[i][j] = if s1[i - 1] == s2[j - 1] {
-                1 + dp[i - 1][j - 1]
+pub fn longest_common_subsequence_v2(s1: String, s2: String) -> i32 {
+    let s1 = s1.as_bytes();
+    let s2 = s2.as_bytes();
+    let m = s1.len();
+    let n = s2.len();
+    let mut dp = vec![vec![0; n + 1]; m + 1];
+    for i in (0..m).rev() {
+        for j in (0..n).rev() {
+            dp[i][j] = if s1[i] == s2[j] {
+                1 + dp[i + 1][j + 1]
             } else {
-                dp[i - 1][j].max(dp[i][j - 1])
-            }
+                dp[i][j + 1].max(dp[i + 1][j])
+            };
         }
     }
-    dp[m - 1][n - 1]
+    dp[0][0]
 }
 
 #[test]
