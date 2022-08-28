dab3f01d256f16f4f269be1659ce4e7fd222bdc7
MelvinSam2000 <melvinsamxd@gmail.com>
2022-08-27 20:13:41 -0400

    Revisit 72 Edit Distance

--- src/q72.rs
+++ src/q72.rs
@@ -1,10 +1,40 @@
 /*
+    72 - Edit Distance (Optimized)
+    Time: O(n*m)
+    Space: O(n)
+*/
+pub fn min_distance(word1: String, word2: String) -> i32 {
+    let word1 = word1.as_bytes();
+    let word2 = word2.as_bytes();
+    let m = word1.len() + 1;
+    let n = word2.len() + 1;
+    let mut dp = [vec![0; n], vec![0; n]];
+    dp[0][n - 1] = 1;
+    for j in 0..n {
+        dp[1][j] = (n - j - 1) as i32;
+    }
+    for i in (0..m - 1).rev() {
+        for j in (0..n - 1).rev() {
+            dp[0][j] = if word1[i] == word2[j] {
+                dp[1][j + 1]
+            } else {
+                1 + dp[1][j].min(dp[0][j + 1]).min(dp[1][j + 1])
+            };
+        }
+        dp[1][n - 1] += 2;
+        dp.swap(0, 1);
+    }
+
+    dp[1][0]
+}
+
+/*
     72 - Edit Distance
     Time: O(n*m)
     Space: O(n*m)
-    Note: Probably space can be optimized to O(min(n, m))
+    Note: Space can be optimized to O(m)
 */
-pub fn min_distance(word1: String, word2: String) -> i32 {
+pub fn min_distance_v2(word1: String, word2: String) -> i32 {
     let word1 = word1.as_bytes();
     let word2 = word2.as_bytes();
     let m = word1.len() + 1;
@@ -25,10 +55,11 @@ pub fn min_distance(word1: String, word2: String) -> i32 {
             };
         }
     }
+
     dp[0][0] as i32
 }
 
-pub fn print_2d_array<T: std::fmt::Display>(arr: &Vec<Vec<T>>) {
+pub fn print_2d_array<T: std::fmt::Display>(arr: &[Vec<T>]) {
     let m = arr.len();
     let n = arr[0].len();
     for i in 0..m {
