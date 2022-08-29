a5668fc8e553a9e3d584296ea03e84cd74a56dc4
MelvinSam2000 <melvinsamxd@gmail.com>
2022-08-28 23:17:41 -0400

    22 Generate Parentheses

---
+++ src/q22.rs
@@ -0,0 +1,22 @@
+/*
+    22 - Generate Parentheses
+    Time: O(2^n)
+    Space: O(n)
+*/
+pub fn generate_parenthesis(n: i32) -> Vec<String> {
+    let mut res = vec![];
+    helper(1, 0, n, "(".to_owned(), &mut res);
+    res
+}
+
+fn helper(open: i32, close: i32, n: i32, output: String, res: &mut Vec<String>) {
+    if open == close && open == n {
+        res.push(output.clone());
+    }
+    if open != n {
+        helper(open + 1, close, n, output.clone() + "(", res);
+    }
+    if open > close {
+        helper(open, close + 1, n, output + ")", res);
+    }
+}
