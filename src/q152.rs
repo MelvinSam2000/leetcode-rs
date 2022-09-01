5cb78298ffa11e106749cd556569eec8bf48eb53
MelvinSam2000 <melvinsamxd@gmail.com>
2022-08-31 22:11:51 -0400

    Revisited 152 Max Product Subarray

--- src/q152.rs
+++ src/q152.rs
@@ -1,25 +1,40 @@
 /*
     152 - Maximum Product Subarray
     Time: O(n)
-    Space: O(n)
+    Space: O(1)
 */
 pub fn max_product(nums: Vec<i32>) -> i32 {
-    use std::cmp::max;
-    use std::cmp::min;
     let n = nums.len();
-
-    let mut acc_max = vec![0; n];
-    let mut acc_min = vec![0; n];
-    acc_max[0] = nums[0];
-    acc_min[0] = nums[0];
-
+    let mut dp = [[nums[0]; 2], [nums[0]; 2]];
+    let mut res = nums[0];
     for i in 1..n {
-        let pmax = acc_max[i - 1] * nums[i];
-        let pmin = acc_min[i - 1] * nums[i];
-        let xmax = max(pmax, pmin);
-        let xmin = min(pmax, pmin);
-        acc_max[i] = max(nums[i], xmax);
-        acc_min[i] = min(nums[i], xmin);
+        dp[0][0] = nums[i].min(nums[i] * dp[1][0]).min(nums[i] * dp[1][1]);
+        dp[0][1] = nums[i].max(nums[i] * dp[1][0]).max(nums[i] * dp[1][1]);
+        res = res.max(dp[0][1]);
+        dp[1] = dp[0];
     }
-    acc_max.into_iter().max().unwrap()
+    res
+}
+
+/*
+    152 - Maximum Product Subarray (Unoptimized/Readable)
+    Time: O(n)
+    Space: O(n)
+*/
+pub fn max_product_v2(nums: Vec<i32>) -> i32 {
+    let n = nums.len();
+    let mut dp = [vec![0; n], vec![0; n]];
+    dp[0][0] = nums[0];
+    dp[1][0] = nums[0];
+    let mut res = nums[0];
+    for i in 1..n {
+        dp[0][i] = nums[i]
+            .min(nums[i] * dp[0][i - 1])
+            .min(nums[i] * dp[1][i - 1]);
+        dp[1][i] = nums[i]
+            .max(nums[i] * dp[0][i - 1])
+            .max(nums[i] * dp[1][i - 1]);
+        res = res.max(dp[1][i]);
+    }
+    res
 }
