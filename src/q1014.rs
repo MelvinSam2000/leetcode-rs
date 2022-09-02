8652df9d962a0c7a6785249969473bf727badf69
MelvinSam2000 <melvinsamxd@gmail.com>
2022-09-02 13:03:07 -0400

    Revisit 1014 Best Sightseeing pair

--- src/q1014.rs
+++ src/q1014.rs
@@ -3,14 +3,13 @@
     Time: O(n)
     Space: O(1)
 */
-pub fn max_score_sightseeing_pair(values: Vec<i32>) -> i32 {
-    let n = values.len();
-    let mut l = values[n - 1] - (n as i32 - 1);
+pub fn max_score_sightseeing_pair(a: Vec<i32>) -> i32 {
+    let n = a.len();
+    let mut dp = a[n - 1] - n as i32 + 1;
     let mut res = 0;
-    for i in (0..=n - 2).rev() {
-        let r = values[i] + i as i32;
-        l = l.max(values[i + 1] - (i as i32 + 1));
-        res = res.max(l + r);
+    for i in (0..n - 1).rev() {
+        dp = dp.max(a[i + 1] - i as i32 - 1);
+        res = res.max(a[i] + i as i32 + dp);
     }
     res
 }
