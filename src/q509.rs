00db499a0cd004e3490f2803c88c06179cfafcd9
MelvinSam2000 <melvinsamxd@gmail.com>
2022-09-01 22:28:30 -0400

    509 Ebic Fibonacci

--- src/q509.rs
+++ src/q509.rs
@@ -16,6 +16,27 @@ pub fn fib(n: i32) -> i32 {
     dp[0]
 }
 
+/*
+    509 - Fibonacci Number (Ebic version)
+    Time: O(1)
+    Space: O(1)
+*/
+pub fn fib_ebic(n: i32) -> i32 {
+    const K: usize = 40;
+    static FIB: [i32; K] = {
+        let mut fib = [0; K];
+        fib[0] = 0;
+        fib[1] = 1;
+        let mut i = 2;
+        while i < K {
+            fib[i] = fib[i - 1] + fib[i - 2];
+            i += 1;
+        }
+        fib
+    };
+    FIB[n as usize]
+}
+
 #[test]
 fn t1() {
     let expected = vec![0, 1, 1, 2, 3, 5, 8, 13, 21];
