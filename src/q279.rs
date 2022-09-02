a30619860a8b37cb59c5e63548a554f939fa1d4c
MelvinSam2000 <melvinsamxd@gmail.com>
2022-09-01 23:16:40 -0400

    279 Perfect Squares

--- src/q279.rs
+++ src/q279.rs
@@ -1,37 +1,21 @@
 /*
     279 - Perfect Squares
-    Time: O(n^2)
+    Time: O(n^(3/2))
     Space: O(n)
 */
 pub fn num_squares(n: i32) -> i32 {
-    const K: usize = 95;
-    static SQUARES: [usize; K] = {
-        let mut out = [0; K];
-        let mut i = 0;
-        while i < K {
-            out[i] = i.pow(2) as usize;
-            i += 1;
-        }
-        out
-    };
-
     let n = n as usize;
-    let mut dp = (1..=n + 1).collect::<Vec<_>>();
+    let mut dp = (1..=n as i32 + 1).collect::<Vec<_>>();
 
-    for &square in &SQUARES {
-        if square > n {
-            break;
-        }
-        dp[square] = 1;
+    for i in 0..=(n as f64).sqrt() as usize {
+        dp[i.pow(2)] = 1;
     }
 
     for i in 1..=n {
-        for &square in &SQUARES {
-            if square > i {
-                break;
-            }
-            dp[i] = dp[i].min(dp[square] + dp[i - square]);
+        for j in 0..=(i as f64).sqrt() as usize {
+            let j = j.pow(2);
+            dp[i] = dp[i].min(dp[j] + dp[i - j]);
         }
     }
-    dp[n] as i32
+    dp[n]
 }
