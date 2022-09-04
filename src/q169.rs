5ebd50436d36c5396a11f521b4fb1601e642f0e5
MelvinSam2000 <melvinsamxd@gmail.com>
2022-09-03 23:17:32 -0400

    Revisited 169 Majority Element

--- src/q169.rs
+++ src/q169.rs
@@ -2,19 +2,19 @@
     169 - Majority Element
     Time: O(n)
     Space: O(1)
+    Note: Uses Boyer Moore Voting Algorithm
 */
 pub fn majority_element(nums: Vec<i32>) -> i32 {
-    let mut res = nums[0];
-    let mut count = 1;
-    for num in nums.into_iter().skip(1) {
-        if num == res {
+    let mut res = 0;
+    let mut count = 0;
+    for elem in nums {
+        if count == 0 {
+            res = elem;
+            count += 1;
+        } else if elem == res {
             count += 1;
         } else {
             count -= 1;
-        }
-        if count < 0 {
-            res = num;
-            count = 1;
         }
     }
     res
