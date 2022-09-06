aff75bca2fa995e5b6175eaf1f4134aa99fa86a6
MelvinSam2000 <melvinsamxd@gmail.com>
2022-09-06 19:44:35 -0400

    Revisit 42 Trapping Rain Water

--- src/q42.rs
+++ src/q42.rs
@@ -2,7 +2,7 @@
     42 - Trapping Rain Water
     Time: O(n)
     Space: O(1)
-    Note: water_formula => water[i] = max(max(L[i], R[i]) - height[i], 0)
+    Note: water_formula => water[i] = max(min(L[i], R[i]) - height[i], 0)
     where L[i] = max(height[..i]), R[i] = max(height[i+1..])
 */
 pub fn trap(height: Vec<i32>) -> i32 {
@@ -26,6 +26,27 @@ pub fn trap(height: Vec<i32>) -> i32 {
         }
     }
     total_water
+}
+
+/*
+    42 - Trapping Rain Water (Unoptimized)
+    Time: O(n)
+    Space: O(n) (Can be O(1) with 2 pointer method)
+*/
+pub fn trap_v2(height: Vec<i32>) -> i32 {
+    let n = height.len();
+    let mut l = vec![0; n];
+    let mut r = vec![0; n];
+    l[0] = -1;
+    r[n - 1] = -1;
+
+    for i in 1..n {
+        l[i] = l[i - 1].max(height[i - 1]);
+    }
+    for i in (0..n - 1).rev() {
+        r[i] = r[i + 1].max(height[i + 1]);
+    }
+    (0..n).map(|i| 0.max(l[i].min(r[i]) - height[i])).sum()
 }
 
 #[test]
